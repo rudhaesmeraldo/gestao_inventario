8cd9080da82e122febacf1a546685b55df82ff94
rudha <rudhaesmeraldo@gmail.com>
2025-03-13 17:06:44 -0300

    🐛 fix: correção no Django Admin

--- inventario/admin.py
+++ inventario/admin.py
@@ -5,9 +5,11 @@ from .models import Produto, MovimentacaoEstoque
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'quantidade', 'preco', 'fornecedor', 'atualizado_em')
     search_fields = ('nome', 'fornecedor')
+    list_filter = ('fornecedor', 'atualizado_em')
 
 @admin.register(MovimentacaoEstoque)
-class MovimentacaoEstoque(admin.ModelAdmin):
+class MovimentacaoEstoqueAdmin(admin.ModelAdmin):
     list_display = ('produto', 'tipo', 'quantidade', 'data')
-    search_fields = ('produto__nome',)
+    search_fields = ('produto__nome', 'tipo', 'motivo')
     list_filter = ('tipo', 'data')
+
