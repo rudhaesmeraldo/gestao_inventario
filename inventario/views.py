3937c7b2969542a2433c6aa3af9b523063b081bc
rudha <rudhaesmeraldo@gmail.com>
2025-03-15 01:58:10 -0300

    ✨ feat: implementação de todas as views

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Produto, MovimentacaoEstoque
+from .forms import ProdutoForm
 
-# Create your views here.
+
+def estoque_view(request):
+    produtos = Produto.objects.all()
+    return render(request, 'index.html', {'produtos': produtos})
+
+def movimentacoes_view(request):
+    movimentacoes = MovimentacaoEstoque.objects.all()
+    return render(request, 'movimentacoes.html', {'movimentacoes':movimentacoes})
+
+def adicionar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('estoque')
+    
+    else:
+        form = ProdutoForm()
+    
+    return render(request, 'produto_form.html', {'form': form, 'titulo':'Adicionar Produto'})
+
+def registrar_saida(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+
+    if request.method == 'POST':
+        quantidade = int(request.POST.get('quantidade', 0))
+        if quantidade > 0 and quantidade <= produto.quantidade:
+            produto.quantidade -= quantidade
+            produto.save()
+            MovimentacaoEstoque.objects.create(
+                produto=produto, tipo='saida', quantidade=quantidade, responsavel=request.user
+            )
+            return redirect('estoque')
+    
+    return render(request, 'produto_form.html', {'produto': produto, 'titulo': 'Registrar Saída'}) 
+        
